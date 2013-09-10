9fe991d3c2d1890a14ba29ec12de5e88ca53f694
lukesunca <lukesunca@gmail.com>
2013-09-10 15:34:24 -0700

    Update parsehtml.py

--- parsehtml.py
+++ parsehtml.py
@@ -18,49 +18,34 @@ def getContent(url):
 
 
 def getCategory(bs4_content):
-	# i=0
 	for cat in bs4_content.findAll("a", attrs={"class":"f14"}):
 		url_cat = url_cats + cat.get('href')
-		#print cat.string, url_cats
 		time.sleep(5)
 		cat_content = getContent(url_cat)
-		#print "cat_content len="+`len(cat_content)`
 		getFood(cat.string, cat_content) #pass category name
-		# if i==0:
-			# break
 
 def getFood(catstr, bs4_content):
-	# i=0
 	for allcontent in bs4_content.findAll("ul",attrs={"class":"l3"}):
 		for food in allcontent.findAll("a", attrs={"target":"_blank"}):
 			url_food_postfix = food.get('href').split("/")[1]
 			url_food = url_cats + url_food_postfix
-			#url_food = url_food.strip()
-			#print food.string, url_food
-			time.sleep(5)
+			time.sleep(5) #sleep 5 seconds to avoid of 502 Bad Gateway error
 			food_content = getContent(url_food)
-			#print "food_content len="+`len(food_content)`
 			getNutrition(catstr, food.string, food_content)
-			#print url_food, url_food.__len__()
-			# if i==0:
-				# break
 
 
 def getNutrition(catstr, foodstr, bs4_content):
 	#Cat,Food,Calorie,Protein,Fat,Carbonhydrate,Fiber,Vitamin A,Carotene,....
 	finalstring = catstr + ',' + foodstr
-	#print "nutrition content len="+`bs4_content.__len__()`
-	# print content.string
 	for allcontent in bs4_content.findAll("table",attrs={"class":"bx"}):
 		global k
 		if k==0:
-			title_string = u"类别,名称,"
+			title_string = "类别,名称,"
 			for title in allcontent.findAll("th"):
 				title_string += "," + title.text.strip()
 			k=k+1
 			print title_string
-		#finalstring+=allcontent.string
-	# 	#print finalstring
+
 		for nutrition in allcontent.findAll("td"):
 			# k=k+1
 			finalstring += ',' + nutrition.text.strip()
