def96efc70e51a84a347f9301bda27bd2cb809ef
lukesunca <lukesunca@gmail.com>
2013-09-11 16:44:44 -0700

    Update parsehtml.py

--- parsehtml.py
+++ parsehtml.py
@@ -13,14 +13,14 @@ url_cats = "http://yingyang.911cha.com/"
 k=0
 
 def getContent(url):
-	html_page = urllib2.urlopen(url)
+	html_page = urllib2.urlopen(url).read()
 	return BeautifulSoup(html_page)
 
 
 def getCategory(bs4_content):
 	for cat in bs4_content.findAll("a", attrs={"class":"f14"}):
 		url_cat = url_cats + cat.get('href')
-		time.sleep(5)
+		time.sleep(10)
 		cat_content = getContent(url_cat)
 		getFood(cat.string, cat_content) #pass category name
 
@@ -29,29 +29,26 @@ def getFood(catstr, bs4_content):
 		for food in allcontent.findAll("a", attrs={"target":"_blank"}):
 			url_food_postfix = food.get('href').split("/")[1]
 			url_food = url_cats + url_food_postfix
-			time.sleep(5) #sleep 5 seconds to avoid of 502 Bad Gateway error
 			food_content = getContent(url_food)
 			getNutrition(catstr, food.string, food_content)
 
 
 def getNutrition(catstr, foodstr, bs4_content):
-	#Category,Food Name,Calorie,Protein,Fat,Carbonhydrate,Fiber,Vitamin A,Carotene,....
-	finalstring = catstr + ',' + foodstr
+	#######Cat,Food,Calorie,Protein,Fat,Carbonhydrate,Fiber,Vitamin A,Carotene,....
+	finalstring = "\n" + catstr + ',' + foodstr
 	for allcontent in bs4_content.findAll("table",attrs={"class":"bx"}):
-		
-		#csv title
 		global k
 		if k==0:
-			title_string = "类别,名称,"
+			title_string = u"类别,名称,"
 			for title in allcontent.findAll("th"):
 				title_string += "," + title.text.strip()
 			k=k+1
 			print title_string
-
-		#csv data
+			finalstring += title_string
 		for nutrition in allcontent.findAll("td"):
-			finalstring += ',' + nutrition.text.strip()
-	print finalstring
+			finalstring += "," + nutrition.text.strip()
+	f = open('myfile.csv','a')
+	f.write(finalstring.encode('utf-8'))
 
 #print all contents
 getCategory(getContent(url_cats))
